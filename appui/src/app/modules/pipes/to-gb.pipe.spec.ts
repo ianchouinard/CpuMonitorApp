e8dc38f807156ae206fbfc9975f0314bf9f848a1
ianchouinard <ianchouinard@gmasil.com>
2019-01-15 09:37:04 -0500

    removed old

--- appui/src/app/modules/pipes/to-gb.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToGBPipe } from './to-gb.pipe';
-
-describe('ToGBPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToGBPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
